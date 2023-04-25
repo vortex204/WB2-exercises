7d410fe4dea1cc8bccca181e95f185c335f5c1fd
vortex204 <jespinal1193@gmail.com>
2023-04-25 11:22:25 -0400

    fixed

--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -1,10 +1,10 @@
-// formula tip amount = bill amount x (tip percentage / 100)
-// assign value to bill amount and tipPercentage
-let billAmount = 50;
+// formula tip amount = Food Bill x (tip percentage / 100)
+// assign value to foodBill and tipPercentage
+let foodBill = 50;
 let tipPercentage = 20;
 
 //calculate tip amount 
-let tipAmount = billAmount * (tipPercentage / 100);
+let tipAmount = foodBill * (tipPercentage / 100);
 
 // print results
-console.log("the tip amount is: $" + tipAmount.toFixed(2));
+console.log("the tip on $" + foodBill.toFixed(2) + " food bill is $" + tipAmount.toFixed(2));
