b338f723b89149a9073f5457d4f7195943e7f0ad
vortex204 <jespinal1193@gmail.com>
2023-04-25 10:50:50 -0400

    rectangle

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,9 @@
+// assign values to length and width 
+let length = 10;
+let width = 5;
+
+//calculate area 
+let area = length * width;
+
+//print results 
+console.log("the area of the rectangle is: "+ area);
