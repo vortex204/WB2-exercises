db86150f0b0a10d52935cec0b05f45ec37d654d7
vortex204 <jespinal1193@gmail.com>
2023-04-25 11:09:55 -0400

    circle

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,9 @@
+// formula Area = π x radius²
+// assign value to radius 
+let radius = 5;
+
+// calculate area 
+let area = Math.PI * Math.pow(radius, 2);
+
+//print results
+console.log("The area of the circle is: " + area.toFixed(2));
