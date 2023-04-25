e968cb4ea05905fb758111b822b39f9e85d4d3e7
vortex204 <jespinal1193@gmail.com>
2023-04-25 10:43:53 -0400

    excercise 1

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,12 @@
+// prompt user ofr assets and debt
+let assets = 25000;
+let debt =  8000;
+
+// calculate net worth 
+let networth = assets - debt;
+
+// display net worth 
+console.log("your net worth is " + networth.toFixed(2)); 
+
+
+
